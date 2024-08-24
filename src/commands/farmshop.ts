8296bd0211ed189af53c847eaffc6d1a4964a6a4
aryan9190 <115806023+aryan9190@users.noreply.github.com>
2024-08-24 21:59:56 +0530

    Update farmshop.ts

--- src/commands/farmshop.ts
+++ src/commands/farmshop.ts
@@ -1,19 +1,22 @@
-import { PrismaClient } from '@prisma/client';
-import { Middleware, SlackCommandMiddlewareArgs } from '@slack/bolt';
-import { showBuyModal } from '../genstoreBuy';
-import { getFarmItems } from '../utils/farmItems';
-
 export const handleFarmshopCommand: (prisma: PrismaClient) => Middleware<SlackCommandMiddlewareArgs> =
   (prisma) => async ({ command, ack, say }) => {
     await ack();
     const farmItems = await getFarmItems();
     const [action, itemName, price] = command.text.split(' ');
 
-    if (action === 'buy') {
+    if (action === 'setprice') {
+      if (adminIds.includes(command.user_id)) {
+        if (farmItems[itemName] && !isNaN(price)) {
+          farmItems[itemName].price = parseFloat(price);
+          await say(`Price for ${farmItems[itemName].emoji} set to :gp:${price}.`);
+        } else {
+          await say('Invalid item or price.');
+        }
+      } else {
+        await say('You do not have permission to use this command.');
+      }
+    } else if (action === 'buy') {
       await showBuyModal(command.trigger_id, say);
-    } else if (action === 'setprice' && farmItems[itemName] && !isNaN(price)) {
-      farmItems[itemName].price = parseFloat(price);
-      await say(`Price for ${farmItems[itemName].emoji} set to :gp:${price}.`);
     } else {
       await say('Invalid command. Use `/farmshop buy` or `/farmshop setprice <item> <price>`.');
     }
