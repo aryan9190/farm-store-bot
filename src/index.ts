ce4fb32596092315142c7905f800a82c51b89d45
aryan9190 <115806023+aryan9190@users.noreply.github.com>
2024-08-28 22:00:48 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,31 @@
 import { App } from '@slack/bolt';
-import { PrismaClient } from '@prisma/client';
-import dotenv from 'dotenv';
-dotenv.config();
+import { sendItemList } from './sendUpdate';
+import './scheduler'; // Importing scheduler to ensure it runs
 
-const prisma = new PrismaClient();
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
   socketMode: true,
-  appToken: process.env.SLACK_APP_TOKEN,
+  appToken: process.env.SLACK_APP_TOKEN
 });
 
-import { handleFarmshopCommand } from './commands/farmshop';
-import { showBuyModal, handleBuyModalSubmit } from './genstoreBuy';
+app.event('app_mention', async ({ event, say }) => {
+  await sendItemList();
+});
 
-app.command('/farmshop', handleFarmshopCommand(prisma));
-app.view('buy_modal', handleBuyModalSubmit(prisma));
+app.action(/buy_.*/, async ({ action, ack, say }) => {
+  await ack();
+  const itemTag = action.action_id.replace('buy_', '');
+  await say(`You bought a ${itemTag.replace('-', '')}`);
+});
+
+app.action(/sell_.*/, async ({ action, ack, say }) => {
+  await ack();
+  const itemTag = action.action_id.replace('sell_', '');
+  await say(`You sold a ${itemTag.replace('-', '')}`);
+});
 
 (async () => {
   await app.start(process.env.PORT || 3000);
-  console.log('⚡️ Farm Shop Bot is running!');
+  console.log('⚡️ Farm Store Bot is running!');
 })();
